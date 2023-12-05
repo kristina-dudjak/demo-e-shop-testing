d4fcd8b21ae17c651c9577bf7f8a437e8883766d
Kristina Dudjak <dudjakkristina@gmail.com>
2023-12-05 14:58:28 +0100

    feat: add smoke tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://magento.softwaretestingboard.com/'
+    baseUrl: 'https://magento.softwaretestingboard.com/',
+    defaultCommandTimeout: 20000,
+    pageLoadTimeout: 60000
   },
 });
